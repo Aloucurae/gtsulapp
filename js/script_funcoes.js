7a1056b4636f8e2c7f79ed12579d3d9be3d04c5c
alex jonas <alexj.desantanna@gmail.com>
2015-04-24 15:34:21 -0300

    adicionado funcãor de ligar direto do app

--- js/script_funcoes.js
+++ js/script_funcoes.js
@@ -1,12 +1,44 @@
+
+var idconte;
+
 function ampliame(id) {
 	document.getElementById(id).className = "amplia";
+	idcon = 'cont_'+id;
+	idconte = idcon;
+	document.getElementById(idcon).style.display = "block"; 
 
 	 var divs =document.getElementsByClassName("normal"); 
 	 for(var i=0;i<=divs.length;i++) { 
 	
 	 		divs[i].style.display = "none"; 	
-	 			 
+
 	 } 
+}
+
+function voltanormal(){
+
+	 var divs = document.getElementsByClassName('normal'); 	 
+
+	 for(var i=0;i<=divs.length;i++) { 
+	
+	 		divs[i].style.display = "block"; 	 		 
+
+	 } 	  
 
 }
+
+function voltanormal1(){
+
+	document.getElementById(idcon).style.display = "none";
+	var ampliado = document.getElementsByClassName("amplia"); 
+
+	 for(var i=0;i<=ampliado.length;i++) { 
+	 	
+	 		ampliado[i].className = "normal"; 	
+	 		voltanormal();
+	 		
+
+	 } 	 
+}
+
 
