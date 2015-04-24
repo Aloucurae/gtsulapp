7f58e2df6740f070473c9dfaa3a156c0b285ea6f
alex jonas <alexj.desantanna@gmail.com>
2015-04-24 11:54:57 -0300

    selecionar e ampliar

--- js/script_funcoes.js
+++ js/script_funcoes.js
@@ -1,4 +1,12 @@
 function ampliame(id) {
-	alert(id);
-	getElementsByTagName(id);
+	document.getElementById(id).className = "amplia";
+
+	 var divs =document.getElementsByClassName("normal"); 
+	 for(var i=0;i<=divs.length;i++) { 
+	
+	 		divs[i].style.display = "none"; 	
+	 			 
+	 } 
+
 }
+
