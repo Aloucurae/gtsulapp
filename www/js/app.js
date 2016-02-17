52aa37a9129066351500053e44091ce15553cc68
Alex jonas de sant'anna <alexj.desantanna@gmail.com>
2016-02-17 13:04:18 -0200

    indalis
    v1

---
+++ www/js/app.js
@@ -0,0 +1,76 @@
+
+// create the module and name it scotchApp
+var myApp = angular.module('myApp',[]); 
+ 
+myApp.controller('listCtrl', ['$scope', '$http', function ($scope, $http){
+
+	$('#hover').hide();
+	$('#info').hide();
+
+	$('#hover').fadeIn( "slow", function() {
+		    // Animation complete.
+	});
+
+	var dados = JSON.parse(window.localStorage.getItem('dados'));
+
+	var stat=1;
+
+	if (dados) {
+
+		$scope.lugares = dados.lugar;
+    	$scope.categorias = dados.categ;
+    	stat=2;
+    	$('#hover').fadeOut( "slow", function() {
+		    // Animation complete.
+		});
+
+	} 
+
+ 
+    $http.get('http://app.alexjonas.com.br/lista/app.php?stat='+stat)
+
+    	.error(function(data, status) {
+			console.error('Erro ao conectar com o servidor',status);
+			alert('falha ao conectar com o servidor \nErro: '+status+'\nTente novamente mais tarde ou \nEnvie um print da tela para o desenvolvedor');
+		})
+
+	    .success(function(data) {
+
+	    	if (data.stat == 1) {
+
+		    	$scope.lugares = data.lugar;
+		    	$scope.categorias = data.categ;
+
+		    	window.localStorage.setItem('dados', JSON.stringify(data));
+
+		    	$('#hover').fadeOut( "slow", function() {
+				    // Animation complete.
+				});
+			  	console.log('Banco Sicronizado');
+
+	    	}else{
+			  	console.log('Sem Atualização');
+
+	    	}
+
+
+	    });
+
+}]); /**/
+   
+  
+function info(){
+
+	$('#info').fadeIn( "slow", function() {
+	});
+
+}
+
+function info2(){
+
+	$('#info').fadeOut( "slow", function() {
+	});
+
+}
+
+
